b2f88427df46bf40594e9a34177cdf62cbd0cd1e
indap <punmagari@student.swosu.edu>
2016-01-26 23:46:30 -0600

    first commit

---
+++ src/CS1_HW_Prb_1_2.java
@@ -0,0 +1,19 @@
+public class CS1_HW_Prb_1_2 {
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+		System.out.println("Hello Indap!");
+		//HW1
+		// Problem 1.6
+		System.out.println("\nProblem 1.6");
+		System.out.print("1+2+3+4+5+6+7+8+9 is: ");
+		System.out.println(1+2+3+4+5+6+7+8+9);
+		// Problem 1.9
+		System.out.println("\nProblem 1.9");
+		System.out.println("area = width * height");
+		System.out.println("width is 4.5"); 
+		System.out.println("Height is 7.9");
+		System.out.print("area = 4.5*7.9 = ");   
+		System.out.println(4.5*7.9);
+	}
+
+}
